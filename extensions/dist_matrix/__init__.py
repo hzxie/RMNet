49348681c8af3eea6382655af3cd80e7128f0a40
Haozhe Xie <cshzxie@gmail.com>
2020-08-26 09:44:25 +0800

    Enable motion attention in STM.

--- extensions/dist_matrix/__init__.py
+++ extensions/dist_matrix/__init__.py
@@ -2,7 +2,7 @@
 # @Author: Haozhe Xie
 # @Date:   2020-08-17 09:06:16
 # @Last Modified by:   Haozhe Xie
-# @Last Modified time: 2020-08-23 09:18:16
+# @Last Modified time: 2020-08-26 09:43:58
 # @Email:  cshzxie@gmail.com
 
 import torch
@@ -16,10 +16,16 @@ class DistanceMatrixFunction(torch.autograd.Function):
         dist_mtx = dist_matrix.forward(mask, prob_threshold)
         return dist_mtx
 
+    @staticmethod
+    def backward(ctx, grad_dist_mtx):
+        # TODO
+        return torch.ones(grad_dist_mtx.size()).float().cuda(), None
+
 
 class DistanceMatrix(torch.nn.Module):
     def __init__(self):
         super(DistanceMatrix, self).__init__()
 
     def forward(self, mask, prob_threshold=0.5, scale_factor=0.1):
-        return torch.sqrt(DistanceMatrixFunction.apply(mask, prob_threshold)) * scale_factor
+        dist_mtx = torch.sqrt(DistanceMatrixFunction.apply(mask, prob_threshold)) * scale_factor
+        return torch.sigmoid(-dist_mtx) * 2
